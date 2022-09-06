fea618f324a6635cf459872212811d4825e569c0
Oluwaseun Jayeoba <seunjay92@gmail.com>
2022-09-06 07:09:42 +0100

    use connect button from web3uikit

--- components/Header.js
+++ components/Header.js
@@ -1,55 +1,10 @@
-import React, { useEffect } from "react";
-import { useMoralis } from "react-moralis";
+import React from "react";
+import { ConnectButton } from "web3uikit";
 
 const Header = () => {
-  const {
-    enableWeb3,
-    account,
-    isWeb3Enabled,
-    Moralis,
-    deactivateWeb3,
-    isWeb3EnableLoading,
-  } = useMoralis();
-
-  useEffect(() => {
-    if (isWeb3Enabled) return;
-
-    if (typeof window !== undefined) {
-      if (window.localStorage.getItem("connected")) {
-        enableWeb3();
-      }
-    }
-  }, [isWeb3Enabled, enableWeb3]);
-
-  useEffect(() => {
-    Moralis.onAccountChanged((account) => {
-      console.log(`Account changed to ${account}`);
-      if (account === null) {
-        window.localStorage.removeItem("connected");
-        deactivateWeb3();
-      }
-    });
-  });
-
-  const connect = async () => {
-    await enableWeb3();
-
-    if (typeof window !== undefined) {
-      window.localStorage.setItem("connected", "injected");
-    }
-  };
   return (
     <div>
-      {account ? (
-        <div>
-          Connected to {account.slice(0, 6)}...
-          {account.slice(account.length - 4)}
-        </div>
-      ) : (
-        <button onClick={connect} disabled={isWeb3EnableLoading}>
-          Connect
-        </button>
-      )}
+      <ConnectButton moralisAuth={false} />
     </div>
   );
 };
