c10627ce06e054479dbb7374df09ba1e6ba526dd
Oluwaseun Jayeoba <seunjay92@gmail.com>
2022-11-12 08:54:03 +0100

    change

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useWeb3Contract } from "react-moralis";
+
+const LotteryEntrance = () => {
+  const { runContractFunction } = useWeb3Contract();
+
+  
+  return <div>LotteryEntrance</div>;
+};
+
+export default LotteryEntrance;
